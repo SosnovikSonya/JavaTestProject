84e4a861d1f796a72655e46ad1ad4207afc3ded7
SosnovikSonya <sonya_sosnovik2001@mail.ru>
2021-03-29 17:42:34 +0300

    update parallel test example to TestNG project

--- TestNG_SimpleExample/src/test/java/ParallelTest.java
+++ TestNG_SimpleExample/src/test/java/ParallelTest.java
@@ -1,7 +1,25 @@
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ParallelTest {
-    @Test(threadPoolSize = 5, invocationCount = 10)
-    public void testCase1() {
+//    @Test(threadPoolSize = 5, invocationCount = 10)
+//    public void testCase1() {
+//    }
+
+    private WebDriver driver;
+
+    @Test
+    public void Test1(){
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        driver.navigate().to("https://testng.org/doc/");
+    }
+
+    @Test
+    public void Test2(){
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        driver.navigate().to("https://testng.org/doc/");
     }
 }
